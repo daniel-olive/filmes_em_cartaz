be2c0dee7a7bb5845a8eb33fa2e7a6ccc3306f79
Daniel Oliveira <daniel.olive.dev@gmail.com>
2024-05-11 00:33:19 -0300

    Configurações do Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/filmes_em_cartaz",
+  base: "/filmes_em_cartaz_simples",
 })
